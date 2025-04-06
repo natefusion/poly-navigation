5f816febc9cfbd42e9007ff726ff68a8965edbea
Nathan Piel <nthnpiel@gmail.com>
2025-04-06 19:26:59 -0400

    remove unnecessary lines

--- run.sh
+++ run.sh
@@ -1,14 +1,3 @@
-cd osm
-
-if [ -e "polycampus.geojson" ]; then
-    node filter-search-queries.js > output.geojson
-else
-    echo "You didn't export polycampus.geojson from JOSM!"
-    exit 1
-fi
-
-cd ..
-
 docker run -d --rm -it -v $(pwd)/tileserver-data:/data -p 8080:8080 maptiler/tileserver-gl:latest
 npm run dev &
 
