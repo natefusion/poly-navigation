fb57d179b2b97bf30c5b4a8aa7a771ed4ac92e1f
Nathan Piel <nthnpiel@gmail.com>
2025-04-06 16:39:54 -0400

    run docker in background properly

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ fi
 
 cd ..
 
-docker run --rm -it -v $(pwd)/tileserver-data:/data -p 8080:8080 maptiler/tileserver-gl:latest &
-
+docker run -d --rm -it -v $(pwd)/tileserver-data:/data -p 8080:8080 maptiler/tileserver-gl:latest
 npm run dev &
+
+wait
