54ddda5fdcea20262dd425fd4f1901e104953b82
Nathan Piel <nthnpiel@gmail.com>
2025-04-06 19:25:59 -0400

    fix build step

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ fi
 
 cd osm
 
-if [ -e "polycampus.osm.pbf" ]; then
-    osmium renumber -o polycampus.osm.pbf polycampus.osm --overwrite
-    docker run -it --rm -v $(pwd):/data ghcr.io/systemed/tilemaker:master /data/polycampus.osm.pbf /data/polycampus.mbtiles
-    mv -f polycampus.mbtiles ../tileserver-data/
-else
-    echo "You didn't export polycampus.osm.pbf from JOSM! You need to download the plugin to export .pbf files first!"
-    exit 1
-fi
+osmium renumber -o polycampus.osm.pbf polycampus.osm --overwrite
+docker run -it --rm -v $(pwd):/data ghcr.io/systemed/tilemaker:master /data/polycampus.osm.pbf /data/polycampus.mbtiles
+mv -f polycampus.mbtiles ../tileserver-data/
+
+osmium export -o polycampus.geojson polycampus.osm --overwrite
+node filter-search-queries.js > output.geojson
+
+
 
 
 
