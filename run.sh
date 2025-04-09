87e25c4b9a0724024146ff072c2d9224bccfeef5
Nathan Piel <nthnpiel@gmail.com>
2025-04-09 16:01:32 -0400

    make it so ctrl-c exits the docker containers

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-docker run -d --rm -it -v $(pwd)/tileserver-data:/data -p 8080:8080 maptiler/tileserver-gl:latest
-docker run -d -t -i -p 5000:5000 -v $(pwd)/osm:/data ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/polycampus.osrm
+docker compose up &
 npm run dev &
 
 wait
